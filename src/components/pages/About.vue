d95055a3bd411eda117e26931367a3af2025ec9f
小澤晶徳 <akinening@gmail.com>
2020-10-25 15:16:14 +0900

    build

--- src/components/pages/About.vue
+++ src/components/pages/About.vue
@@ -8,22 +8,8 @@
     <vision-area />
 
     <section class="mt-60">
-      <h2 class="c-title">職歴</h2>
+      <h2 class="c-title">Experience</h2>
       <ul class="c-entries">
-        <li>
-          <h3 class="c-entry-title">
-            <span>
-              <a href="https://www.wantedly.com/org/31599">面白法人カヤック</a>
-            </span>
-          </h3>
-          <p class="font-13">
-            フロントエンドエンジニア
-          </p>
-          <p class="c-description">Lobi 及び Lobi Tournament のフロントエンド開発</p>
-          <p class="c-line" style="background-image:url(https://de7iszmjjjuya.cloudfront.net/assets/linethrough-bg-ca1a0157a77da0bca2ace712e4d6037a7aa1dec69db54d1491c4e7918a33855a.gif);">
-            <span>2018-04 - 2019-08</span>
-          </p>
-        </li>
         <li>
           <h3 class="c-entry-title">
           <span itemprop="affiliation">
@@ -38,12 +24,20 @@
           <span>2019-11 - </span>
           </p>
         </li>
-      </ul>
-    </section>
-
-    <section class="mt-60">
-      <h2 class="c-title">学歴</h2>
-      <ul class="c-entries">
+        <li>
+          <h3 class="c-entry-title">
+            <span>
+              <a href="https://www.wantedly.com/org/31599">面白法人カヤック</a>
+            </span>
+          </h3>
+          <p class="font-13">
+            フロントエンドエンジニア
+          </p>
+          <p class="c-description">Lobi 及び Lobi Tournament のフロントエンド開発</p>
+          <p class="c-line" style="background-image:url(https://de7iszmjjjuya.cloudfront.net/assets/linethrough-bg-ca1a0157a77da0bca2ace712e4d6037a7aa1dec69db54d1491c4e7918a33855a.gif);">
+            <span>2018-04 - 2019-08</span>
+          </p>
+        </li>
         <li>
           <h3 class="c-entry-title">
             <span>
