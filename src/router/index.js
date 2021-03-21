625ec900105e714fea87808f7eb00d33aeb6454e
Akinen <akinening@gmail.com>
2021-03-21 13:51:16 +0900

    historyモード

--- src/router/index.js
+++ src/router/index.js
@@ -7,6 +7,7 @@ import About from '@/components/pages/About'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     { path: '/', name: 'Index', component: Index },
     { path: '/works', name: 'Works', component: Works },
