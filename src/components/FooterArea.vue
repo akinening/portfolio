83535de4cc8cdc7f3cae18955e7e55a133abea43
Akinen <akinening@gmail.com>
2021-04-21 20:46:34 +0900

    footerを横に

--- src/components/FooterArea.vue
+++ src/components/FooterArea.vue
@@ -1,12 +1,14 @@
 <template>
   <footer class="footer">
     <p class="title">Contact</p>
-    <a class="contact" href="https://www.facebook.com/ackiena" target="_blank" rel="noopener noreferrer">
-      Facebook
-    </a>
-    <a class="contact" href="https://twitter.com/_akinen" target="blank" rel="noopener noreferrer">
-      Twitter
-    </a>
+    <div class="contacts">
+      <a class="contact" href="https://www.facebook.com/ackiena" target="_blank" rel="noopener noreferrer">
+        Facebook
+      </a>
+      <a class="contact" href="https://twitter.com/_akinen" target="blank" rel="noopener noreferrer">
+        Twitter
+      </a>
+    </div>
     <p class="footer__copy">©︎ 2021 Akinen</p>
   </footer>
 </template>
@@ -21,13 +23,12 @@ export default {
 .footer
   padding-top 40px
   text-align center
-  background-color white
+  color white
+  background-color rgb(36, 40, 42)
 
   &__copy
-    width 100%
-    background-color #80d8d0
     font-weight 500
-    margin-top 32px
+    padding 16px 0
 
 .title
   font-size 24px
@@ -49,14 +50,18 @@ export default {
   line-height 19px
   vertical-align middle
 
+.contacts
+  display flex
+  justify-content center
+  margin 0 10px
+
 .contact
-  display block
-  margin 0 auto 20px
-  width 280px
+  display inline-block
+  margin 0 10px 10px
+  width 200px
   padding 12px 30px
   color rgb(36, 40, 42)
   background-color white
-  border 1px solid rgb(36, 40, 42)
   border-radius 4px
   font-size 16px
   font-weight 500
