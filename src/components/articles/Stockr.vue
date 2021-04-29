b139ee24a4ddaf2101270a00dd0eb72247bacff5
Akinen <akinening@gmail.com>
2021-04-29 15:04:43 +0900

    flow画像を追加

--- src/components/articles/Stockr.vue
+++ src/components/articles/Stockr.vue
@@ -21,17 +21,25 @@
       <a href="https://inside.bldt.jp/entry/2020/11/26/104143" target="_blank" class="top-btn">取り組み事例</a>
     </p>
 
-    <h2 class="title">デザインシステムの構築</h2>
+    <h2 class="title">1→10のデザイン</h2>
+    <p class="description">
+      <img class="image image--2col" :src="stockr_flow1" alt="基本機能の改善">
+      <img class="image image--2col" :src="stockr_flow2" alt="マーケ施策">
+      現在執筆中です。
+      定性・定量調査と、それに基づく改善を行いました。
+    </p>
+
+    <h2 class="title">マネタイズ</h2>
+    <p class="description">
+      現在執筆中です。
+    </p>
+
+    <h2 class="title">その他</h2>
     <p class="description">
       FlutterはMaterial Designを標準サポートしていることから、Material Designに寄せ、iOSとAndroidでデザインは極力同じにすることとしました。フォントについては英字はRoboto, 日本語はNoto Sans JPを使用しています。ただしログイン周りや通知の権限要求など、OSに依存する部分は別で対応しています。<br>
       <img class="image image--2col" :src="stockr_color" alt="カラー" loading="lazy">
       <img class="image image--2col" :src="stockr_component" alt="コンポーネントの例" loading="lazy">
       フロントエンドエンジニアとしての知識を活かし、共通化できる要素はコンポーネント化してからエンジニアに共有するなど、職種間での意思疎通がスムーズになるよう取り組んでいます。
-    </p>
-
-    <h2 class="title">定性・定量調査と、それに基づく改善</h2>
-    <p class="description">
-      現在執筆中です‥
     </p>
   </section>
 </template>
@@ -44,7 +52,9 @@ export default {
       stockr_color: require('@/assets/image/stockr-color.png'),
       stockr_component: require('@/assets/image/stockr-component.png'),
       stockr_ogp: require('@/assets/image/stockr.png'),
-      stockr_process: require('@/assets/image/stockr-process.png')
+      stockr_process: require('@/assets/image/stockr-process.png'),
+      stockr_flow1: require('@/assets/image/stockr-flow1.png'),
+      stockr_flow2: require('@/assets/image/stockr-flow2.png')
     }
   }
 }
