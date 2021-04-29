d0e028945a606076f15b59e08b80f6e02ce6750a
Akinen <akinening@gmail.com>
2021-04-29 18:51:55 +0900

    starlightを追加

--- src/components/FooterArea.vue
+++ src/components/FooterArea.vue
@@ -35,21 +35,6 @@ export default {
   font-weight 600
   margin-bottom 40px
 
-.icon
-.contact
-  color rgba(36, 40, 42, 1.0)
-  transition all 0.2s
-
-  &:hover
-    color rgba(36, 40, 42, 0.6)
-
-.icon-mail
-  display inline-block
-  margin 0 4px 2px 0
-  color rgb(36, 40, 42)
-  line-height 19px
-  vertical-align middle
-
 .contacts
   display flex
   justify-content center
@@ -60,9 +45,13 @@ export default {
   margin 0 10px 10px
   width 200px
   padding 12px 30px
-  color rgb(36, 40, 42)
+  color rgba(36, 40, 42, 1.0)
   background-color white
   border-radius 4px
   font-size 16px
   font-weight 500
+  transition all 0.2s
+
+  &:hover
+    color rgba(36, 40, 42, 0.6)
 </style>
