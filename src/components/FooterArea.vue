c20d3d0e0d43ddaa78bf72e1cee696b887a3689c
Akinen <akinening@gmail.com>
2021-06-04 01:11:19 +0900

    footerにアイコンをつける

--- src/components/FooterArea.vue
+++ src/components/FooterArea.vue
@@ -3,10 +3,12 @@
     <p class="title">Contact</p>
     <div class="contacts">
       <a class="contact" href="https://www.facebook.com/ackiena" target="_blank" rel="noopener noreferrer">
-        Facebook
+        <img class="contact__img" :src="facebook" alt="facebook">
+        <p>Facebook</p>
       </a>
       <a class="contact" href="https://twitter.com/_akinen" target="blank" rel="noopener noreferrer">
-        Twitter
+        <img class="contact__img" :src="twitter" alt="twitter">
+        <p>Twitter</p>
       </a>
     </div>
     <p class="footer__copy">©︎ 2021 Akinen</p>
@@ -15,7 +17,13 @@
 
 <script>
 export default {
-  name: 'FooterArea'
+  name: 'FooterArea',
+  data () {
+    return {
+      facebook: require('@/assets/image/sns/facebook.svg'),
+      twitter: require('@/assets/image/sns/twitter.svg')
+    }
+  }
 }
 </script>
 
@@ -28,12 +36,12 @@ export default {
 
   &__copy
     font-weight 500
-    padding 16px 0
+    padding 24px 0 16px
 
 .title
   font-size 24px
   font-weight 600
-  margin-bottom 40px
+  margin-bottom 32px
 
 .contacts
   display flex
@@ -50,8 +58,15 @@ export default {
   border-radius 4px
   font-size 16px
   font-weight 500
-  transition all 0.2s
+  transition all 0.15s
+
+  &__img
+    opacity 1
+    transition all 0.15s
 
   &:hover
     color rgba(36, 40, 42, 0.6)
+
+    > .contact__img
+      opacity 0.6
 </style>
