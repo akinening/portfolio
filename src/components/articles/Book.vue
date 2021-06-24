7b0bb9f84156fb9bc919dac6655545b36018d72c
Akinen <akinening@gmail.com>
2021-06-24 10:44:19 +0900

    build

--- src/components/articles/Book.vue
+++ src/components/articles/Book.vue
@@ -3,9 +3,12 @@
     <img class="image" :src="book_ogp" alt="現場から見る デザインとエンジニアリングの間">
 
     <p class="description">
-      現在、技術書展に向けて準備中です。
+      「デザイン読書日和」にて初版を頒布。現在は技術書典に向けて準備中です。
       <span class="link">
-        <a href="https://booth.pm/ja/items/2470035" target="_blank" class="link__button">旧版はこちら</a>
+        <a href="https://booth.pm/ja/items/2470035" target="_blank" class="link__button">BOOTH</a>
+      </span>
+      <span class="link">
+        <a href="https://techbookfest.org/product/5108267816583168" target="_blank" class="link__button">技術書典11</a>
       </span>
     </p>
   </section>
