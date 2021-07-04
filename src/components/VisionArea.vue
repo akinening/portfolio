1eee98ef9161b634d26b9a8de3058655c7dab1e6
Akinen <akinening@gmail.com>
2021-07-04 04:13:47 +0000

    visionに画像を追加

--- src/components/VisionArea.vue
+++ src/components/VisionArea.vue
@@ -2,23 +2,12 @@
   <section>
     <h2 class="c-title mt-80">Vision / Philosophy</h2>
     <ul class="visionaly">
-      <li class="vision c-shadow">
-        <p class="vision__title">Service Design</p>
-        <p class="vision__description">
-          幅広い媒体のデザイン経験をもとに、一貫したサービスデザインに取り組んでいます。「人から愛されるデザイン」がテーマ。
-        </p>
-      </li>
-      <li class="vision c-shadow">
-        <p class="vision__title">Design × Engineering</p>
-        <p class="vision__description">
-          元エンジニアとしての経験を活かし、他職種との意思疎通の橋渡し役となることで、より良いサービス開発を目指します。
-        </p>
-      </li>
-      <li class="vision c-shadow">
-        <p class="vision__title">Contribution to Society</p>
-        <p class="vision__description">
-          縁の下の力持ち。デザインを通じて、より多くの人々の暮らしを豊かにすることで、社会に貢献します。
-        </p>
+      <li class="vision c-shadow"  v-for="(vision, num) in visions" :key="`vision-${num}`">
+        <img class="vision__image" :src="vision.image" :alt="vision.title">
+        <div class="detail">
+          <p class="detail__title">{{ vision.title }}</p>
+          <p class="detail__description">{{ vision.description }}</p>
+        </div>
       </li>
     </ul>
   </section>
@@ -29,6 +18,23 @@ export default {
   name: 'VisionArea',
   data () {
     return {
+      visions: [
+        {
+          image: require('@/assets/image/vision1.png'),
+          title: 'Service Design',
+          description: '幅広い媒体のデザイン経験をもとに、一貫したサービスデザインに取り組んでいます。「人から愛されるデザイン」がテーマ。'
+        },
+        {
+          image: require('@/assets/image/vision2.png'),
+          title: 'Design × Engineering',
+          description: '元エンジニアとしての経験を活かし、他職種との意思疎通の橋渡し役となることで、より良いサービス開発を目指します。'
+        },
+        {
+          image: require('@/assets/image/vision3.png'),
+          title: 'Contribution to Society',
+          description: '縁の下の力持ち。デザインを通じて、より多くの人々の暮らしを豊かにすることで、社会に貢献します。'
+        }
+      ]
     }
   }
 }
@@ -46,11 +52,10 @@ export default {
 
 .vision
   width 100%
-  min-height 108px
-  padding 20px
   background-color white
   border-radius 6px
   box-sizing border-box
+  overflow hidden
 
   &:not(:last-child)
     margin 0 20px 0 0
@@ -58,6 +63,12 @@ export default {
   @media (max-width: 768px)
     &:not(:last-child)
         margin 0 0 20px 0
+
+  &__image
+    width 100%
+
+.detail
+  padding 20px
 
   &__title
     font-size 20px
