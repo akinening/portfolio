c8429474ed53a24119d2a2a6fe73068531b30ded
Akinen <akinening@gmail.com>
2022-04-16 11:19:46 +0000

    build

--- src/components/articles/Tmnf.vue
+++ src/components/articles/Tmnf.vue
@@ -1,5 +1,7 @@
 <template>
   <section class="skill mt-80 mb-30">
+    <img class="image" src="@/assets/image/tmnf.png" alt="TMNF">
+
     <p class="description">
       UI/UXデザイナーとして、損害保険事業のPoC・新規事業創出に携わっています。<br>
       保険サービスの多様化や、スマホを通した保険の需要拡大から、保険業界では新規事業の創出が課題になっています。２人目の中途デザイナーとして入社し、本部のプロダクトオーナーと協力しながら、実際の顧客ニーズに沿ったプロダクトデザインを行っています。
@@ -8,11 +10,12 @@
     <h2 class="title">略歴</h2>
     <p class="description">
       2022.04 ~<br>
-      ・Apple Watchを用いた疾病予防アプリ<br>
+      <b>Apple Watchを用いた疾病予防アプリ</b><br>
+      ・iOSネイティブアプリのUIデザイン： Apple本社のデザイナーに相談<br>
+      ・Time To Value： 初回オンボーディングのステップ数（操作回数）を極力減らす。プロトタイピングでフローを確認<br>
       <br>
       2021.11 ~ 2022.03<br>
-      ・BCP（事業継続計画）策定支援サービスのUIデザイン<br>
-
+      <b>BCP（事業継続計画）策定支援サービスのUIデザイン</b><br>
     </p>
     <p class="description">
       ※コンプライアンスの観点から詳細は非公開です。
