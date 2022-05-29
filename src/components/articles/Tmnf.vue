b30ebd87b25faeab60769421dc8d8458e2fb911b
Akinen <akinening@gmail.com>
2022-05-29 13:38:53 +0000

    やや追記

--- src/components/articles/Tmnf.vue
+++ src/components/articles/Tmnf.vue
@@ -11,9 +11,20 @@
     <p class="description">
       2022.04 ~<br>
       顧客のヘルスケアをサポートするiOSネイティブアプリのUX/UIデザインを担当しています。<br>
-       <img class="image image--w80" src="@/assets/image/apple watch.jpg" alt="Apple Watch">
+      <img class="image image--w80" src="@/assets/image/apple watch.jpg" alt="Apple Watch">
+      【課題】<br>
+      【結果】<br>
+      【取り組み】<br>
       ・iOSネイティブアプリのUIデザイン： Apple本社のデザイナーに相談<br>
       ・Time To Value： 初回オンボーディングのステップ数（操作回数）を極力減らす。プロトタイピングでフローを確認<br>
+    </p>
+
+    <h2 class="title">ベンチャー文化の導入</h2>
+    <p class="description">
+      ・DXの一環<br>
+      ・Slackに個別のtimesチャンネルを作成し、業務での気づきやデザインについての話題を共有<br>
+      ・<br>
+      ・
     </p>
 
     <h2 class="title">その他</h2>
@@ -23,8 +34,6 @@
       2021.11 ~ 2022.03<br>
       製造業・小売・介護など、幅広い業種に対応した事業継続計画を策定できるWebサービスのUIデザインを担当しました。
       <img class="image image--w80" src="@/assets/image/tmnf/bcsp.png" alt="BCSP">
-    </p>
-    <p class="description">
       ※コンプライアンスの観点から詳細は非公開です。
     </p>
   </section>
