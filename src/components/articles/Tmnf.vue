ad83508dccd5ceeb7b84c1a94b20d2eb9447b4df
Akinen <akinening@gmail.com>
2022-06-30 05:15:21 +0000

    build

--- src/components/articles/Tmnf.vue
+++ src/components/articles/Tmnf.vue
@@ -4,44 +4,40 @@
 
     <p class="description">
       UI/UXデザイナーとして、損害保険事業のPoC・新規事業創出に携わっています。<br>
-      保険サービスの多様化や、スマホを通した保険の需要拡大から、保険業界では新規事業の創出が課題になっています。２人目の中途デザイナーとして入社し、本部のプロダクトオーナーと協力しながら、実際の顧客ニーズに沿ったプロダクトデザインを行っています。
+      保険サービスの多様化や、スマホを通した保険の需要拡大から、保険業界では新規事業の創出が課題になっています。２人目の中途デザイナーとして入社し、本部のプロダクトオーナーと協力しながら、実際の顧客ニーズに沿ったプロダクトデザインを行っています。<br>
+      ※コンプライアンスの観点から、案件の詳細は非公開です。
     </p>
 
-    <h2 class="title">Apple Watchを用いた疾病予防アプリ</h2>
+    <h2 class="title">DXの推進</h2>
+    <p class="description">
+      ・Slackにチームメンバーのtimesチャンネル作成を提言し、業務での気づきやデザインについての話題を共有する場をつくった。<br>
+      ・HCD-Netへの登録を提言し、賛助会員として登録。<br>
+      ・イラストの刷新を提言するため、各社のイラストシステムについて調査し、広報部に提言。<br>
+      ・デザイガイドラインの整理・コンポーネント化（現在進行中）<br>
+      ・Figmaの社内導入に向けた比較資料の作成
+      <img class="image image--w80" src="@/assets/image/tmnf/figma.png" alt="Figmaのメリット">
+    </p>
+
+    <h3 class="title title--sub">社内ポータルのリデザイン</h3>
+    <p class="description">
+      秘伝のタレと化した社内ポータルをリデザイン（現在進行中）
+      <img class="image image--w80" src="@/assets/image/tmnf/portal.png" alt="ポータル">
+    </p>
+
+    <h2 class="title">UX/UIデザイン</h2>
+    <br>
+    <h3 class="title title--sub">Apple Watchを用いた疾病予防アプリ</h3>
     <p class="description">
       2022.04 ~<br>
       顧客のヘルスケアをサポートするiOSネイティブアプリのUX/UIデザインを担当しています。<br>
       <img class="image image--w80" src="@/assets/image/apple watch.jpg" alt="Apple Watch">
-      【課題】<br>
-      「お客様の『いざ』をお守りする」という弊社のミッションをもとに、疾病の予兆を事前に検知するアプリケーションの開発に着手しています。
-      Apple Watchをつけた体験はアプリに閉じておらず、日常生活からユーザーストーリーを作成して機能に落とし込む必要があります。
-      また、医療として適切なライティングや表現に注意が必要です。
-      【結果】 →PJ進行中<br>
-      【取り組み】<br>
-      ・Time To Value： 初回オンボーディングのステップ数（操作回数）を極力減らす。プロトタイピングでフローを確認<br>
     </p>
 
-    <h2 class="title">ベンチャー文化の導入</h2>
-    <p class="description">
-      ・Slackにチームメンバーのtimesチャンネル作成を提言し、業務での気づきやデザインについての話題を共有する場をつくった。<br>
-      ・HCD-Netへの賛助会員登録を提言し、現在関係各所と調整中。<br>
-      ・イラストの刷新を提言するため、各社のイラストシステムについて調査。
-    </p>
-
-    <h2 class="title">社内ポータルのリデザイン</h2>
-    <p class="description">
-      秘伝のタレと化した社内ポータルを、現在リデザイン中です。
-      <img class="image image--w80" src="@/assets/image/tmnf/portal.png" alt="ポータル">
-    </p>
-
-    <h2 class="title">その他</h2>
-    <br>
     <h3 class="title title--sub">BCP（事業継続計画）策定支援サービス</h3>
     <p class="description">
       2021.11 ~ 2022.03<br>
       製造業・小売・介護など、幅広い業種に対応した事業継続計画を策定できるWebサービスのUIデザインを担当しました。
       <img class="image image--w80" src="@/assets/image/tmnf/bcsp.png" alt="BCSP">
-      ※コンプライアンスの観点から詳細は非公開です。
     </p>
   </section>
 </template>
