496a346db4cca1f593f99634b57fdd560214a473
Akinen <akinening@gmail.com>
2022-07-01 15:18:24 +0000

    vrchat追加

--- src/components/articles/Logo.vue
+++ src/components/articles/Logo.vue
@@ -5,6 +5,8 @@
     <p class="description">
       DJロゴの制作を担当しました。
       <img class="image image--w80" src="@/assets/image/deneb_process.png" alt="制作過程">
+      作成したロゴは、VRChat上のDJイベントで使われました
+      <img class="image image--w80" src="@/assets/image/logo_vrchat.png" alt="VRChat">
     </p>
   </section>
 </template>
