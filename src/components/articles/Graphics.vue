938ae85cd5067928ebced87ca822be381505829e
Akinen <akinening@gmail.com>
2022-09-21 15:03:22 +0000

    build

--- src/components/articles/Graphics.vue
+++ src/components/articles/Graphics.vue
@@ -2,18 +2,21 @@
   <section class="skill mt-80 mb-30">
     <img class="image" src="@/assets/image/graphics.png" alt="Logo">
 
+    <h3 class="title title--sub">CDジャケットの制作</h3>
     <p class="description">
-      DJロゴの制作を担当しました。
+      ハードコア・ハードダンスコンピ「HARD MANIAXX」のCDジャケットデザインを担当しました。<br>
+      作品を表す主題がないなかで、複数案をつくって絞り込むことでイメージを具体化していきました。
+      <img class="image image--w80" src="@/assets/image/creative/album.png" alt="アルバム">
+    </p>
+
+    <h3 class="title title--sub">DJロゴの制作</h3>
+    <p class="description">
+      DJロゴの依頼を受け制作しました。文字の対称性や可読性、他のロゴと並んだときの印象の残りやすさを意識しています。
       <img class="image image--w80" src="@/assets/image/deneb_process.png" alt="制作過程">
-      作成したロゴは、VRChat上のDJイベントで使われました
+      作成したロゴは、VRChat上のDJイベントで使われました。
       <img class="image image--w80" src="@/assets/image/logo_vrchat.png" alt="VRChat">
     </p>
 
-    <h3 class="title title--sub">CDジャケットのデザイン</h3>
-    <p class="description">
-      詳しい説明をここに入れる
-      <img class="image image--w80" src="@/assets/image/creative/album.png" alt="アルバム">
-    </p>
   </section>
 </template>
 
