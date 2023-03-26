68bab812230240e76ad51a76153e7ae86bd3842f
Akinen <akinening@gmail.com>
2023-03-26 11:24:09 +0000

    starlightとvue-loader.jsを削除

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,11 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import 'vue-loaders/dist/vue-loaders.css'
-import VueLoaders from 'vue-loaders'
-
 import Index from '@/components/pages/Index'
 import WorksDetail from '@/components/pages/WorksDetail'
 import About from '@/components/pages/About'
 import Creative from '@/components/pages/Creative'
 
 Vue.use(Router)
-Vue.use(VueLoaders)
 
 export default new Router({
   routes: [
